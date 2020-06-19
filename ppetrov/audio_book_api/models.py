13586f2284d781cfc61b5b5496d9fe653974ca5f
ppetrov <p.petrov@ucdn.com>
2020-06-19 18:01:00 +0300

    initial

---
+++ ppetrov/audio_book_api/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    category = models.ManyToManyField(Category)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
